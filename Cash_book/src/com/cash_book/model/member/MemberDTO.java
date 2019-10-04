dcd503f6b259a35533a2ea2ee9f04515801b0ac9
Chocobe <nice_10bird@naver.com>
2019-10-05 01:43:23 +0900

    <Chocobe> : Fixed class MemberDTO

--- Cash_book/src/com/cash_book/model/member/MemberDTO.java
+++ Cash_book/src/com/cash_book/model/member/MemberDTO.java
@@ -8,23 +8,35 @@ import com.cash_book.model.GetableAttributeNamesDTO;
 // Static Factory Method Pattern
 // 		(DTO 필드변수 자료형의 정보은닉을 위한 목적)
 public class MemberDTO extends GetableAttributeNamesDTO {
-	private String id;
-	private String pw;
 	private String phone;
+	private String name;
+	private String pw;
 	
+	private static final int PHONE_MAX_LENGTH;
+	private static final int NAME_MAX_LENGTH;
+	private static final int PW_MAX_LENGTH;
+	
+	static {
+		PHONE_MAX_LENGTH = 11;
+		NAME_MAX_LENGTH = 10;
+		PW_MAX_LENGTH = 15;
+	}
 	
 // 생성자
-	private MemberDTO(String _id, String _pw, String _phone) {
+	private MemberDTO(String _phone, String _name, String _pw) {
 		super("MEMBER");
-		this.id = _id;
-		this.pw = _pw;
 		this.phone = _phone;
+		this.name = _name;
+		this.pw = _pw;
 	}
 	
 	
 // Instance Factory
-	public static MemberDTO create(String _id, String _pw, String _phone) {
-		return new MemberDTO(_id, _pw, _phone);
+	public static MemberDTO create(String _phone, String _name, String _pw) {
+		return (_phone.length() > PHONE_MAX_LENGTH) 
+					&& (_name.length() > NAME_MAX_LENGTH)
+					&& (_pw.length() > PW_MAX_LENGTH)
+					? new MemberDTO(_phone, _name, _pw) : null;
 	}
 
 
@@ -32,9 +44,9 @@ public class MemberDTO extends GetableAttributeNamesDTO {
 	@Override
 	public List<String> getAttributeNames() {
 		List<String> names = new ArrayList<String>();
-		names.add("id");
-		names.add("pw");
 		names.add("phone");
+		names.add("name");
+		names.add("pw");
 		
 		return names;
 	}
@@ -44,9 +56,9 @@ public class MemberDTO extends GetableAttributeNamesDTO {
 	@Override
 	public List<String> getAttributeValues() {
 		List<String> values = new ArrayList<String>();
-		values.add(id);
-		values.add(pw);
 		values.add(phone);
+		values.add(name);
+		values.add(pw);
 		
 		return values;
 	}
