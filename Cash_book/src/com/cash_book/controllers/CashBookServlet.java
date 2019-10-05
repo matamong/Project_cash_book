57e7f08cda60bd58ad2d4e11004e11ba028cd6f6
matamong <matamatamong@gmail.com>
2019-10-05 18:00:25 +0900

    <Matamong> : Create Controller&Test index
    <Matamong> : Create Package com.cash_book.cotrollers.action
    
    <Matamong> : Create Controller Class <<CashBookServlet>>
    
    <Matamong> : Create ActionFactory <<CashBookActionFactory>>
    
    <Matamong> : Create Action Interface <<Action>>
    
    <Matamong> : Create <<IndexAction>>
    
    <Matamong> : Create JSPTest File Where /WebContents/JSPTest/
    
    <Matamong> : Create indexTest.jsp Where
    /WebContents/JSPTest/indexTest.jsp
    
    죄송합니다 그때그때 커밋해야하는데 이미 다 만들어버린 뒤에 생각났습니다 어흑 ㅠㅠㅠ

---
+++ Cash_book/src/com/cash_book/controllers/CashBookServlet.java
@@ -0,0 +1,41 @@
+package com.cash_book.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cash_book.controllers.action.Action;
+
+@WebServlet("/CashBook")
+public class CashBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CashBookServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String command = request.getParameter("command");
+		System.out.println("CashBookServlet에서의 요청 : " + command);
+		CashBookActionFactory actionFactory = CashBookActionFactory.getInstance();
+		Action action = actionFactory.getAction(command);
+		if(action != null) {
+			action.execute(request, response);
+		}
+		
+		
+
+	}
+
+}
