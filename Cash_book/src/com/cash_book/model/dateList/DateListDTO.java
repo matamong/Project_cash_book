edca3cdd0548db3e7ccf9f2a648ace46256fc681
Chocobe <nice_10bird@naver.com>
2019-10-05 15:38:29 +0900

    <Chocobe> : Added method getAttributeMap

--- Cash_book/src/com/cash_book/model/dateList/DateListDTO.java
+++ Cash_book/src/com/cash_book/model/dateList/DateListDTO.java
@@ -1,7 +1,9 @@
 package com.cash_book.model.dateList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.cash_book.model.GetableAttributeNamesDTO;
 
@@ -11,13 +13,20 @@ public class DateListDTO extends GetableAttributeNamesDTO {
 	private String phone;
 	private String localDate;	
 	
+	private static final String PHONE_NAME;
+	private static final String LOCAL_DATE_NAME;
+	
 	private static final int PHONE_MAX_LENGTH;
 	private static final int LOCAL_DATE_MAX_LENGTH;
 	
 	static {
+		PHONE_NAME = "PHONE";
+		LOCAL_DATE_NAME = "LOCAL_DATE";
+		
 		PHONE_MAX_LENGTH = 11;
 		LOCAL_DATE_MAX_LENGTH = 8;
 	}
+	
 	
 // 생성자
 	private DateListDTO(String _phone, String _localDate) {
@@ -29,11 +38,13 @@ public class DateListDTO extends GetableAttributeNamesDTO {
 	
 // Instance Factory
 	public static DateListDTO create(String _phone, String _localDate) {
-		return new DateListDTO(_phone, _localDate);
+		return (_phone.length() <= PHONE_MAX_LENGTH)
+					&& (_localDate.length() <= LOCAL_DATE_MAX_LENGTH)
+					? new DateListDTO(_phone, _localDate) : null;
 	}
 	
 	
-// getter Names
+// getter Names ( List<String> )
 	@Override
 	public List<String> getAttributeNames() {
 		List<String> names = new ArrayList<String>();
@@ -44,12 +55,22 @@ public class DateListDTO extends GetableAttributeNamesDTO {
 	}
 	
 
-// getter Values
+// getter Values ( List<String> )
 	@Override
 	public List<String> getAttributeValues() {
 		List<String> values = new ArrayList<String>();
 		values.add(phone);
 		values.add(localDate);
+		
+		return values;
+	}
+	
+	
+// getter Map ( Map<String, String> )
+	public Map<String, String> getAttributeMap() {
+		Map<String, String> values = new HashMap<String, String>();
+		values.put(PHONE_NAME, phone);
+		values.put(LOCAL_DATE_NAME, localDate);
 		
 		return values;
 	}
--- Cash_book/src/com/cash_book/model/member/MemberDTO.java
+++ Cash_book/src/com/cash_book/model/member/MemberDTO.java
@@ -1,7 +1,9 @@
 package com.cash_book.model.member;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.cash_book.model.GetableAttributeNamesDTO;
 
@@ -12,15 +14,24 @@ public class MemberDTO extends GetableAttributeNamesDTO {
 	private String name;
 	private String pw;
 	
+	private static final String PHONE_NAME;
+	private static final String NAME_NAME;
+	private static final String PW_NAME;
+	
 	private static final int PHONE_MAX_LENGTH;
 	private static final int NAME_MAX_LENGTH;
 	private static final int PW_MAX_LENGTH;
 	
 	static {
+		PHONE_NAME = "PHONE";
+		NAME_NAME = "NAME";
+		PW_NAME = "PW";
+		
 		PHONE_MAX_LENGTH = 11;
 		NAME_MAX_LENGTH = 10;
 		PW_MAX_LENGTH = 15;
 	}
+	
 	
 // 생성자
 	private MemberDTO(String _phone, String _name, String _pw) {
@@ -33,14 +44,14 @@ public class MemberDTO extends GetableAttributeNamesDTO {
 	
 // Instance Factory
 	public static MemberDTO create(String _phone, String _name, String _pw) {
-		return (_phone.length() > PHONE_MAX_LENGTH) 
-					&& (_name.length() > NAME_MAX_LENGTH)
-					&& (_pw.length() > PW_MAX_LENGTH)
+		return (_phone.length() <= PHONE_MAX_LENGTH) 
+					&& (_name.length() <= NAME_MAX_LENGTH)
+					&& (_pw.length() <= PW_MAX_LENGTH)
 					? new MemberDTO(_phone, _name, _pw) : null;
 	}
 
 
-// getter Names
+// getter Names ( List<String> )
 	@Override
 	public List<String> getAttributeNames() {
 		List<String> names = new ArrayList<String>();
@@ -52,13 +63,24 @@ public class MemberDTO extends GetableAttributeNamesDTO {
 	}
 	
 	
-// getter Values
+// getter Values ( List<String> )
 	@Override
 	public List<String> getAttributeValues() {
 		List<String> values = new ArrayList<String>();
 		values.add(phone);
 		values.add(name);
 		values.add(pw);
+		
+		return values;
+	}
+	
+	
+// getter Map ( Map<String, String> )
+	public Map<String, String> getAttributeMap() {
+		Map<String, String> values = new HashMap<String, String>();
+		values.put(PHONE_NAME, phone);
+		values.put(NAME_NAME, name);
+		values.put(PW_NAME, pw);
 		
 		return values;
 	}
