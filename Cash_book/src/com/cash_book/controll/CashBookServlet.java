677f560d1ac488a3b6284347e158c59bb26282db
matamong <matamatamong@gmail.com>
2019-10-07 08:58:27 +0900

    Try merge to origin/Chocobe

---
+++ Cash_book/src/com/cash_book/controll/CashBookServlet.java
@@ -0,0 +1,29 @@
+package com.cash_book.controll;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CashBookServlet
+ */
+@WebServlet("/CashBook")
+public class CashBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CashBookServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
